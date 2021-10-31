06d4cb2912f9f5e6d8f1dd6eeef811fc3c81faad
Rafael Duarte <elyouus94@gmail.com>
2021-10-31 09:29:31 -0400

    [UPDATE] cambios de permisos y adición de favicon.icon  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .github/workflows/deploy.yml
    	modified:   .vscode/settings.json
    	modified:   package-lock.json
    	new file:   public/favicon.ico
    	modified:   public/index.html
    	modified:   src/assets/icons/bt_add_to_cart.svg
    	modified:   src/assets/icons/bt_added_to_cart.svg
    	modified:   src/assets/icons/email.svg
    	modified:   src/assets/icons/flechita.svg
    	modified:   src/assets/icons/icon_close.png
    	modified:   src/assets/icons/icon_menu.svg
    	modified:   src/assets/icons/icon_shopping_cart.svg
    	modified:   src/assets/icons/icon_shopping_cart_notification.svg
    	modified:   src/assets/logos/favicon_bota_fora.svg
    	modified:   src/assets/logos/favicon_yard_sale.svg
    	modified:   src/assets/logos/logo_bota_fora.svg
    	modified:   src/assets/logos/logo_yard_sale.svg
    	modified:   src/components/Header.jsx
    	modified:   src/components/Menu.jsx
    	modified:   src/components/Order.jsx
    	modified:   src/components/OrderItem.jsx
    	modified:   src/components/ProductInfo.jsx
    	modified:   src/components/ProductItem.jsx
    	modified:   src/components/ShoppingCartItem.jsx
    	modified:   src/containers/Layout.jsx
    	modified:   src/containers/Login.jsx
    	modified:   src/containers/MyOrder.jsx
    	modified:   src/containers/ProductDetail.jsx
    	modified:   src/containers/ProductList.jsx
    	modified:   src/containers/RecoveryPassword.jsx
    	modified:   src/context/AppContext.js
    	modified:   src/hooks/useGetProducts.js
    	modified:   src/hooks/useInitialState.js
    	modified:   src/index.js
    	modified:   src/pages/Checkout.jsx
    	modified:   src/pages/CreateAccount.jsx
    	modified:   src/pages/Home.jsx
    	modified:   src/pages/Login.jsx
    	modified:   src/pages/MyAccount.jsx
    	modified:   src/pages/NewPassword.jsx
    	modified:   src/pages/NotFound.jsx
    	modified:   src/pages/Orders.jsx
    	modified:   src/pages/PasswordRecovery.jsx
    	modified:   src/pages/SendEmail.jsx
    	modified:   src/routes/App.jsx
    	modified:   src/styles/Checkout.scss
    	modified:   src/styles/CreateAccount.scss
    	modified:   src/styles/Header.scss
    	modified:   src/styles/Login.scss
    	modified:   src/styles/Menu.scss
    	modified:   src/styles/MyAccount.scss
    	modified:   src/styles/MyOrder.scss
    	modified:   src/styles/NewPassword.scss
    	modified:   src/styles/Order.scss
    	modified:   src/styles/OrderInfo.scss
    	modified:   src/styles/OrderItem.scss
    	modified:   src/styles/Orders.scss
    	modified:   src/styles/PasswordRecovery.scss
    	modified:   src/styles/ProductDetail.scss
    	modified:   src/styles/ProductInfo.scss
    	modified:   src/styles/ProductItem.scss
    	modified:   src/styles/ProductList.scss
    	modified:   src/styles/RecoveryPassword.scss
    	modified:   src/styles/SendEmail.scss
    	modified:   src/styles/ShoppingCartItem.scss
    	modified:   src/styles/_var.scss
    	modified:   src/styles/_vars.scss
    	modified:   src/styles/global.css
    	modified:   src/styles/global.scss
    	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/react-shop/'
+        publicPath: '/'
     },
     mode: 'development',
     resolve: {
